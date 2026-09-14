91abcd38cb6c2b32946c821534a4aa06b279edee
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-13 23:20:50 -0400

    Export provider-backed engine bridge

--- residual/engines/__init__.py
+++ residual/engines/__init__.py
@@ -4,10 +4,11 @@ from .router import CapabilityRouter
 from .langgraph_adapter import LangGraphEngine
 from .crewai_adapter import CrewAIEngine
 from .sdk_adapter import ClaudeSDKEngine, OpenAIAssistantsEngine
+from .provider_bridge import ProviderEngineConfig, ProviderExecutionEngine
 
 __all__ = [
     "ContextAssembly", "EngineExecutionReceipt", "EngineHealth", "EngineResult",
     "ExecutionEngine", "TaskSpec", "CapabilityProbeSuite", "ProbeResult",
     "CapabilityRouter", "LangGraphEngine", "CrewAIEngine", "ClaudeSDKEngine",
-    "OpenAIAssistantsEngine",
+    "OpenAIAssistantsEngine", "ProviderEngineConfig", "ProviderExecutionEngine",
 ]
