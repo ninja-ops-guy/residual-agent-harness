95f39f945b385fe88feeffec8e76b6566574ad89
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-13 20:48:30 -0400

    export engine execution receipt

--- residual/engines/__init__.py
+++ residual/engines/__init__.py
@@ -1,7 +1,13 @@
-from .protocol import ContextAssembly,EngineHealth,EngineResult,ExecutionEngine,TaskSpec
-from .probe import CapabilityProbeSuite,ProbeResult
+from .protocol import ContextAssembly, EngineExecutionReceipt, EngineHealth, EngineResult, ExecutionEngine, TaskSpec
+from .probe import CapabilityProbeSuite, ProbeResult
 from .router import CapabilityRouter
 from .langgraph_adapter import LangGraphEngine
 from .crewai_adapter import CrewAIEngine
-from .sdk_adapter import ClaudeSDKEngine,OpenAIAssistantsEngine
-__all__=["ContextAssembly","EngineHealth","EngineResult","ExecutionEngine","TaskSpec","CapabilityProbeSuite","ProbeResult","CapabilityRouter","LangGraphEngine","CrewAIEngine","ClaudeSDKEngine","OpenAIAssistantsEngine"]
+from .sdk_adapter import ClaudeSDKEngine, OpenAIAssistantsEngine
+
+__all__ = [
+    "ContextAssembly", "EngineExecutionReceipt", "EngineHealth", "EngineResult",
+    "ExecutionEngine", "TaskSpec", "CapabilityProbeSuite", "ProbeResult",
+    "CapabilityRouter", "LangGraphEngine", "CrewAIEngine", "ClaudeSDKEngine",
+    "OpenAIAssistantsEngine",
+]
