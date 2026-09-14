ea90e60854d98ad61a14d9510f3704fe34b08ecf
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-13 22:59:32 -0400

    Export assurance execution receipt

--- residual/assurance/__init__.py
+++ residual/assurance/__init__.py
@@ -2,12 +2,12 @@ from .distributed import AuthoritativeLog, QuorumUnavailable, ReplicatedEntry, r
 from .market import MarketDecision, MarketProfile, MarketRequest, VerifiedComputeMarket
 from .orchestration import ExecutionStrategy, OrchestrationTaxController, StrategyEstimate, UtilityWeights
 from .quality import AssuranceClass, VerifierQualityProfile, VerifierQualityRegistry
-from .runtime import AdaptiveAssuranceRuntime, ExecutionOutcome, ExecutionPlan
+from .runtime import AdaptiveAssuranceRuntime, AssuranceExecutionReceipt, ExecutionOutcome, ExecutionPlan
 
 __all__ = [
     "AssuranceClass", "VerifierQualityProfile", "VerifierQualityRegistry",
     "ExecutionStrategy", "OrchestrationTaxController", "StrategyEstimate", "UtilityWeights",
     "MarketDecision", "MarketProfile", "MarketRequest", "VerifiedComputeMarket",
     "AuthoritativeLog", "QuorumUnavailable", "ReplicatedEntry", "require_quorum",
-    "AdaptiveAssuranceRuntime", "ExecutionPlan", "ExecutionOutcome",
+    "AdaptiveAssuranceRuntime", "AssuranceExecutionReceipt", "ExecutionPlan", "ExecutionOutcome",
 ]
