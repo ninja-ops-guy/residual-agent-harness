92a89c4034bd83d9dcd85063adc6acfb58b7c1bc
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-13 20:50:53 -0400

    export async run coordinator

--- residual/async_io/__init__.py
+++ residual/async_io/__init__.py
@@ -1,4 +1,6 @@
+from .coordinator import AsyncRunCoordinator
 from .telemetry import AsyncTelemetryClient
 from .sink import AsyncObservationSink
 from .server import AsyncHTTPServer
-__all__=["AsyncTelemetryClient","AsyncObservationSink","AsyncHTTPServer"]
+
+__all__ = ["AsyncRunCoordinator", "AsyncTelemetryClient", "AsyncObservationSink", "AsyncHTTPServer"]
