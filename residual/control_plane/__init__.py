6956058299dc2e5471f34b59bc8dc52d6cf8b69d
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-14 20:32:46 -0400

    Export control plane governance APIs

--- residual/control_plane/__init__.py
+++ residual/control_plane/__init__.py
@@ -1,4 +1,8 @@
 """RESIDUAL gateway/control-plane primitives (SPEC-GCP-001)."""
 from .models import *
 from .policy import AuthorityPolicy, AmendmentPolicy, RoutingPolicy
-from .transactions import SideEffectTransaction
+from .amendments import AmendmentClassifier
+from .environment import EnvironmentRecord, EnvironmentRegistry
+from .routing import RoutingPreconditionResult, validate_execution_start
+from .transactions import SideEffectTransaction, TransactionCoordinator, TransactionReceipt
+from .invariants import INVARIANT_GRAPH, validate_invariant_graph
