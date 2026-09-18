dfa375f8e8b66c5d3c289819980a47524c95d9ea
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-18 16:26:16 -0400

    feat: expose distributed experiment APIs

---
+++ residual/experiments/__init__.py
@@ -0,0 +1,4 @@
+"""Experimental distributed workflow benchmarks."""
+from .distributed import run_mesh_protocol_benchmark, run_station_distributed_benchmark
+
+__all__ = ["run_mesh_protocol_benchmark", "run_station_distributed_benchmark"]
