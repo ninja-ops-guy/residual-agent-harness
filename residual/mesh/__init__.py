7af5f9f0a129181db1d2dc4bfd4e158bcecb45b3
ninja-ops-guy <222743524+ninja-ops-guy@users.noreply.github.com>
2026-09-18 16:28:39 -0400

    feat: export mesh session coordinator

--- residual/mesh/__init__.py
+++ residual/mesh/__init__.py
@@ -1,3 +1,8 @@
 """Federated mesh module."""
 from .node import MeshChat, MeshIdentity, MeshMessage, MeshMessageKind, MeshNode
-__all__ = ["MeshChat", "MeshIdentity", "MeshMessage", "MeshMessageKind", "MeshNode"]
+from .session import DeliveryReceipt, MeshSession
+
+__all__ = [
+    "DeliveryReceipt", "MeshChat", "MeshIdentity", "MeshMessage",
+    "MeshMessageKind", "MeshNode", "MeshSession",
+]
